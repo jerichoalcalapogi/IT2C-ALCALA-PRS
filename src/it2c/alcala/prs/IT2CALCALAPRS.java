4340348b058de2c0caf6df46e827f52813bbd386
dray <misadray3@gmail.com>
2024-10-09 14:41:09 +0800

    updated

--- src/it2c/alcala/prs/IT2CALCALAPRS.java
+++ src/it2c/alcala/prs/IT2CALCALAPRS.java
@@ -8,15 +8,15 @@ public class IT2CALCALAPRS {
  public void addCitizen(){
         Scanner sc = new Scanner(System.in);
         config conf = new config();
-        System.out.print(" First Name: ");
+        System.out.print (" First Name: ");
         String fname = sc.next();
-        System.out.print(" Last Name: ");
+        System.out.print (" Last Name: ");
         String lname = sc.next();
-        System.out.print(" Purok: ");
+        System.out.print (" Purok: ");
         String purok = sc.next();
-        System.out.print(" Contact number: ");
+        System.out.print (" Contact number: ");
         String contact = sc.next();
-        System.out.print(" Status: ");
+        System.out.print( " Status: ");
         String status = sc.next();
 
         String sql = "INSERT INTO citizen (f_name, l_name, e_purok,s_contact, e_status) VALUES (?, ?, ?, ?,?)";
@@ -31,27 +31,82 @@ public class IT2CALCALAPRS {
        Scanner sc=new Scanner(System.in);
        String response;
        do{
-           System.out.println("1. ADD");
-           System.out.println("2. VIEW");
-           System.out.println("3. UPDATE");
-           System.out.println("4. DELETE");
-           System.out.println("5. EXIT");
+           System.out.println ("1. ADD");
+           System.out.println ("2. VIEW");
+           System.out.println ("3. UPDATE");
+           System.out.println ("4. DELETE");
+           System.out.println ("5. EXIT");
            
-           System.out.println("Enter Action");
+           System.out.println("Enter Action:");
             int action =sc.nextInt();
+            IT2CALCALAPRS purok= new IT2CALCALAPRS ();
             switch(action){
                 case 1:
-           IT2CALCALAPRS purok= new IT2CALCALAPRS ();
-           purok.addCitizen();
+                     
+                     purok.addCitizen();
            break;
+                case 2:
+                     
+                     purok.viewCitizen();
+                case 3:
+                    
+                     purok.viewCitizen();
+                     purok.updateCitizen(); 
+                case 4:
+                    
+                     purok.viewCitizen();
+                     purok.deleteCitizen(); 
+                     purok.viewCitizen();
             }
             System.out.println("Continue?(yes/no):");
             response=sc.next();
        }while(response.equals("yes"));
             System.out.println("Thank you for using");
               
-           
-           
+             
             
 }
-}
+    
+    private void viewCitizen() {
+        String citizenQuery = "SELECT * FROM citizen";
+        String [] citizenHeaders = {"ID","First Name","Last Name","Purok", "Contact","Status"};
+        String[] citizenColumns = {"s_id", "f_name", "l_name","e_purok", "s_contact", "e_status"};
+       config conf = new config();
+        conf.viewRecords(citizenQuery, citizenHeaders, citizenColumns);
+    }
+    
+    private void updateCitizen(){
+
+     Scanner sc= new Scanner(System.in);   
+        System.out.println  ("Enter citizen ID:");
+        int id = sc.nextInt();
+        System.out.println ("Enter new First Name:");
+        String nfname= sc.next();
+        System.out.println ("Enter new LastName:");
+        String nlname= sc.next();
+        System.out.println ("Enter new Purok:");
+        String nfpurok= sc.next();
+        System.out.println ("Enter new Contact:");
+        String ncontact= sc.next(); 
+        System.out.println ("Enter new Status:");
+        String nstat= sc.next();
+
+
+        String qry = "UPDATE citizen SET f_name = ?,l_name = ?, e_purok = ?, s_contact = ?, e_status = ? WHERE s_id = ?";
+
+        config conf = new config();
+        conf.updateRecord(qry, nfname,nlname,nfpurok,ncontact,nstat,id);
+
+       }
+   private void deleteCitizen(){
+
+        Scanner sc= new Scanner(System.in);   
+           System.out.println("Enter citizen ID to delete:");
+           int id = sc.nextInt();
+
+           String sqlDelete = "DELETE FROM citizen  WHERE s_id=?";
+           config conf = new config();
+           conf.deleteRecord(sqlDelete, id);
+       }
+
+   }
